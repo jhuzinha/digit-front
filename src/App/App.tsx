ab4dc7a113e995173806055a8daef1773cbda34b
Jhuliana <jholeanasantos@gmail.com>
2022-09-28 14:47:33 -0300

    feat: basic of project, template with routes

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import AppRoutes from "../Routers/Router";
+
+function App() {
+  return (
+    <>
+      <AppRoutes /> 
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/Routers/Router.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+
+export default function AppRoutes(): any{
+    <BrowserRouter>
+        <Routes> 
+            <Route path='/' element = '{}' />
+            <Route path='/login' element= '{}' />
+            <Route path='/register' element= '{}' />
+            <Route path='/profile' element= '{}' />
+            <Route path='/create' element= '{}' />
+        </Routes>
+    </BrowserRouter>
+}
